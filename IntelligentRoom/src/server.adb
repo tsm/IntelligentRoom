86ad32091274afe26542e680c78f976c7bf03812
Smilasek <smilasek@gmail.com>
2013-01-19 23:34:57 +0100

    server changes

--- IntelligentRoom/src/server.adb
+++ IntelligentRoom/src/server.adb
@@ -25,6 +25,7 @@ procedure Server is
       name 		: Unbounded_String;
       active_from	: Time;
       active_to		: Time;
+      brightness	: Integer;
       active_now	: Boolean;
    end record;
 
@@ -58,61 +59,83 @@ procedure Server is
             a_from_m : constant String := AWS.Parameters.Get (Params, "a_from_m");
             a_to_h : constant String := AWS.Parameters.Get (Params, "a_to_h");
             a_to_m : constant String := AWS.Parameters.Get (Params, "a_to_m");
-            a_force : constant String := AWS.Parameters.Get (Params, "a_force");
+            def_value : constant String := AWS.Parameters.Get (Params, "def_value");
             a_f_h,a_t_h : Hour;
             a_f_m,a_t_m : Minute;
             active_from,active_to : Time;
-            active : Boolean := False;
+            def_val : Integer;
          begin
-            if a_from_h = "" then a_f_h :=Positive'Value("0");else a_f_h:=Positive'Value(a_from_h); end if;
-            if a_from_m = "" then a_f_m :=Positive'Value("0");else a_f_m:=Positive'Value(a_from_m); end if;
-            if a_to_h = "" then a_t_h :=Positive'Value("0");else a_t_h:=Positive'Value(a_to_h); end if;
-            if a_to_m = "" then a_t_m :=Positive'Value("0");else a_t_m:=Positive'Value(a_to_m); end if;
-            if a_force = "1" then active := True; else active := False; end if;
-            active_from := (a_f_h,a_f_m);
-            active_to := (a_t_h,a_t_m);
+            if clientsMap.contains(To_Unbounded_String(client)) then
+               return AWS.Response.Build ("text/html", "Podany klient juz istnieje!");
+            else
+               if a_from_h = "" then a_f_h :=Positive'Value("0");else a_f_h:=Positive'Value(a_from_h); end if;
+               if a_from_m = "" then a_f_m :=Positive'Value("0");else a_f_m:=Positive'Value(a_from_m); end if;
+               if a_to_h = "" then a_t_h :=Positive'Value("0");else a_t_h:=Positive'Value(a_to_h); end if;
+               if a_to_m = "" then a_t_m :=Positive'Value("0");else a_t_m:=Positive'Value(a_to_m); end if;
+               if def_value = "" then def_val := 350; else def_val := Positive'Value(def_value); end if;
+               active_from := (a_f_h,a_f_m);
+               active_to := (a_t_h,a_t_m);
 
+               -- dodawanie klientow do mapy
+               clientsMap.Insert(To_Unbounded_String(client),(To_Unbounded_String(client),active_from,active_to,def_val,False) );
 
-            --dorobic parsowanie czasu
-            Put_Line ("Klient:"&client&" from:"&a_from_h&", to:"&a_to_h&" force"&a_force);
-
-            clientsMap.Insert(To_Unbounded_String(client),(To_Unbounded_String(client),active_from,active_to,active) );
-
-            return AWS.Response.Build ("text/html", "<p>Klient:"&client&"<br>"
+               return AWS.Response.Build ("text/html", "<p>Klient:"&client&"<br>"
                                        &"Aktywne od: "&a_from_h&":"&a_from_m&"<br>"
                                        &"Aktywne do: "&a_to_h&":"&a_to_m&"<br>"
-                                       &"Wymuszenie: "&a_force);
+                                       &"Jasnosc: "&def_value);
+            end if;
          end;
       elsif URI = "/read" then
          declare
             client : constant String := AWS.Parameters.Get (Params, "client");
             config : Client_Configuration;
          begin
-            config := clientsMap.Element(To_Unbounded_String(client));
-          --  if config then
-          --     return AWS.Response.Build ("text/html", "<p>Nie znaleziono klienta!");
-          --  else
+            if clientsMap.contains(To_Unbounded_String(client)) then
+               config := clientsMap.Element(To_Unbounded_String(client));
                return AWS.Response.Build ("text/html", "<p>Klient:"&config.name&"<br>"
                                        &"Aktywne od: "&Integer'Image(config.active_from.t_hour)&":"&Integer'Image(config.active_from.t_minute)&"<br>"
                                        &"Aktywne do: "&Integer'Image(config.active_to.t_hour)&":"&Integer'Image(config.active_to.t_minute)&"<br>"
-                                          &"Wymuszenie: "&Boolean'Image(config.active_now));
-          --  end if;
+                                       &"Jasnosc: "&Integer'Image(config.brightness)&"<br>"
+                                          &"Aktywne: "&Boolean'Image(config.active_now));
+            else
+               return AWS.Response.Build ("text/html", "<p>Nie znaleziono klienta!");
+            end if;
          end;
+      elsif URI = "/force" then
+          declare
+            client : constant String := AWS.Parameters.Get (Params, "client");
+            value : constant String := AWS.Parameters.Get (Params, "value");
+            config : Client_Configuration;
+         begin
+            if clientsMap.contains(To_Unbounded_String(client)) then
+               config := clientsMap.Element(To_Unbounded_String(client));
+               if value = "1" then config.active_now := True; else config.active_now := False; end if;
+               clientsMap.replace(To_Unbounded_String(client),config);
+               return AWS.Response.Build ("text/html", "<p>Klient:"&config.name&"<br>"
+                                       &"Aktywne od: "&Integer'Image(config.active_from.t_hour)&":"&Integer'Image(config.active_from.t_minute)&"<br>"
+                                       &"Aktywne do: "&Integer'Image(config.active_to.t_hour)&":"&Integer'Image(config.active_to.t_minute)&"<br>"
+                                       &"Jasnosc: "&Integer'Image(config.brightness));
+            else
+               return AWS.Response.Build ("text/html", "<p>Nie znaleziono klienta!");
+            end if;b
+
+         end;
+      elsif URI = "/setLight" then
+         return AWS.Response.Build ("text/html", "<p>Ustawiam swiatlo !");
       else
          return AWS.Response.Build ("text/html", "<p>Hello world !");
       end if;
 
    end CallBack;
 
+    ----------------------------- KOD SERWERA  ---------------------------------
 begin
-   Put_Line
-     ("Server active : http://localhost:8181/");
-
    AWS.Server.Start(TheServer, "IntelligentRoomServer",
                     Max_Connection => 30,
                     Port => 8181,
                     Callback => CallBack'Unrestricted_Access);
+   Put_Line
+     ("Server active : http://localhost:8181/");
    Put_Line ("Press 'Q' key to quit.");
    AWS.Server.Wait (AWS.Server.Q_Key_Pressed);
-
 end Server;
