14061ad1fa5cc87df8a1ee20e6a477e0de86743e
Smilasek <smilasek@gmail.com>
2013-01-20 18:01:03 +0100

    Server logic

--- IntelligentRoom/src/server.adb
+++ IntelligentRoom/src/server.adb
@@ -8,6 +8,8 @@ with Ada.Strings.Hash;
 with Ada.Containers.Hashed_Maps; use Ada.Containers;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with AWS.Parameters;
+with AWS.Messages;
+with AWS.MIME;
 
 procedure Server is
 
@@ -15,20 +17,34 @@ procedure Server is
    subtype Hour is Integer range 0 .. 23;
    subtype Minute is Integer range 0 .. 59;
 
+   -- Czas (HH:MM)
    type Time is record
       t_hour	: Hour;
       t_minute 	: Minute;
    end record;
 
+   function "<" (time1,time2 : IN Time) return Boolean IS
+   begin
+      IF time1.t_hour < time2.t_hour THEN
+         return True;
+      ELSIF time1.t_hour = time2.t_hour AND time1.t_minute < time2.t_minute THEN
+         return True;
+      ELSE
+         return False;
+      END IF;
+   end "<";
+
    -- Konfiguracja Klienta
    type Client_Configuration is record
-      name 		: Unbounded_String;
-      active_from	: Time;
-      active_to		: Time;
-      brightness	: Integer;
-      active_now	: Boolean;
+      name 		: Unbounded_String;  	-- nazwa klienta
+      active_from	: Time; 		-- aktywnosc od
+      active_to		: Time;			-- aktywnosc do
+      brightness	: Integer;		-- ustawienie jasnosci klienta
+      active_now	: Boolean;		-- czy aktywny teraz
+      force		: Boolean;		-- czy jest wymuszenie
    end record;
 
+   -- Funkcja hashujaca mape
    function Hash_Func(Key : Unbounded_String) return Ada.Containers.Hash_Type is
    begin
       return Ada.Strings.Hash(To_String(Key));
@@ -66,8 +82,9 @@ procedure Server is
             def_val : Integer;
          begin
             if clientsMap.contains(To_Unbounded_String(client)) then
-               return AWS.Response.Build ("text/html", "Podany klient juz istnieje!");
+               return AWS.Response.Build (AWS.MIME.Text_Plain, "Podany klient juz istnieje!");
             else
+               -- przpyspisanie wartosci
                if a_from_h = "" then a_f_h :=Positive'Value("0");else a_f_h:=Positive'Value(a_from_h); end if;
                if a_from_m = "" then a_f_m :=Positive'Value("0");else a_f_m:=Positive'Value(a_from_m); end if;
                if a_to_h = "" then a_t_h :=Positive'Value("0");else a_t_h:=Positive'Value(a_to_h); end if;
@@ -77,12 +94,9 @@ procedure Server is
                active_to := (a_t_h,a_t_m);
 
                -- dodawanie klientow do mapy
-               clientsMap.Insert(To_Unbounded_String(client),(To_Unbounded_String(client),active_from,active_to,def_val,False) );
+               clientsMap.Insert(To_Unbounded_String(client),(To_Unbounded_String(client),active_from,active_to,def_val,False,False) );
 
-               return AWS.Response.Build ("text/html", "<p>Klient:"&client&"<br>"
-                                       &"Aktywne od: "&a_from_h&":"&a_from_m&"<br>"
-                                       &"Aktywne do: "&a_to_h&":"&a_to_m&"<br>"
-                                       &"Jasnosc: "&def_value);
+               return AWS.Response.Build (AWS.MIME.Text_Plain, "OK");
             end if;
          end;
       elsif URI = "/read" then
@@ -92,13 +106,13 @@ procedure Server is
          begin
             if clientsMap.contains(To_Unbounded_String(client)) then
                config := clientsMap.Element(To_Unbounded_String(client));
-               return AWS.Response.Build ("text/html", "<p>Klient:"&config.name&"<br>"
-                                       &"Aktywne od: "&Integer'Image(config.active_from.t_hour)&":"&Integer'Image(config.active_from.t_minute)&"<br>"
-                                       &"Aktywne do: "&Integer'Image(config.active_to.t_hour)&":"&Integer'Image(config.active_to.t_minute)&"<br>"
+               return AWS.Response.Build ("text/html", "<p>Klient: "&config.name&"<br>"
+                                       &"Aktywne od: "&Hour'Image(config.active_from.t_hour)&" :"&Minute'Image(config.active_from.t_minute)&"<br>"
+                                       &"Aktywne do: "&Hour'Image(config.active_to.t_hour)&" :"&Minute'Image(config.active_to.t_minute)&"<br>"
                                        &"Jasnosc: "&Integer'Image(config.brightness)&"<br>"
                                           &"Aktywne: "&Boolean'Image(config.active_now));
             else
-               return AWS.Response.Build ("text/html", "<p>Nie znaleziono klienta!");
+               return AWS.Response.Build (AWS.MIME.Text_Plain, "Nie znaleziono klienta!");
             end if;
          end;
       elsif URI = "/force" then
@@ -111,19 +125,70 @@ procedure Server is
                config := clientsMap.Element(To_Unbounded_String(client));
                if value = "1" then config.active_now := True; else config.active_now := False; end if;
                clientsMap.replace(To_Unbounded_String(client),config);
-               return AWS.Response.Build ("text/html", "<p>Klient:"&config.name&"<br>"
-                                       &"Aktywne od: "&Integer'Image(config.active_from.t_hour)&":"&Integer'Image(config.active_from.t_minute)&"<br>"
-                                       &"Aktywne do: "&Integer'Image(config.active_to.t_hour)&":"&Integer'Image(config.active_to.t_minute)&"<br>"
-                                       &"Jasnosc: "&Integer'Image(config.brightness));
+               return AWS.Response.Build (AWS.MIME.Text_Plain, "OK");
             else
-               return AWS.Response.Build ("text/html", "<p>Nie znaleziono klienta!");
-            end if;b
+               return AWS.Response.Build (AWS.MIME.Text_Plain, "Nie znaleziono klienta!");
+            end if;
 
          end;
       elsif URI = "/setLight" then
-         return AWS.Response.Build ("text/html", "<p>Ustawiam swiatlo !");
+         declare
+            client : constant String := AWS.Parameters.Get (Params, "client");
+            value : constant String := AWS.Parameters.Get (Params, "value");
+            t_h : constant String := AWS.Parameters.Get (Params, "t_h");
+            t_m : constant String := AWS.Parameters.Get (Params, "t_m");
+            config : Client_Configuration;
+            now : Time;
+            brightness, result : Integer;
+            temp,t1,t2 : Float;
+         begin
+            if clientsMap.contains(To_Unbounded_String(client)) then
+               --odczytanie parametrow
+               now := (Hour'Value(t_h),Minute'Value(t_m));
+               brightness := Positive'Value(value);
+               config := clientsMap.Element(To_Unbounded_String(client));
+               -- update zmiennej active_now jesli natrafiono na przedzial czasowy
+               IF config.active_from < config.active_to THEN
+                  IF now = config.active_from OR (config.active_from < now AND now < config.active_to) THEN
+                     config.active_now := True;
+                  ELSE
+                     config.active_now := False;
+                  END IF;
+               ELSIF config.active_from = config.active_to THEN
+                  config.active_now := False;
+               ELSE
+                  IF now = config.active_from
+                    OR ((config.active_from < now AND now < (23,59)) OR now = (23,59))
+                    OR (now = (0,0) OR ((0,0) < now AND now < config.active_to)) THEN
+                     config.active_now := True;
+                  ELSE
+                     config.active_now := False;
+                  END IF;
+               END IF;
+               clientsMap.replace(To_Unbounded_String(client),config);
+
+               -- warunek sterowania oswietleniem
+               IF config.active_now OR config.force THEN
+                  t1 := float(brightness);
+               	  t2 := float(config.brightness);
+                  IF t1 >= t2-50.0 THEN
+                     temp := 0.0;
+                  ELSE
+                     temp := (1.0 - (t1 / t2)) * 100.0;
+                  END IF;
+                  result := Integer(temp);
+                  return AWS.Response.Build (AWS.MIME.Text_Plain, Integer'Image(result));
+               ELSE
+                  return AWS.Response.Build (AWS.MIME.Text_Plain, "Sterowanie oswietleniem jest nieaktywne!");
+               END IF;
+            else
+               return AWS.Response.Build (AWS.MIME.Text_Plain, "Nie znaleziono klienta!");
+            end if;
+         end;
       else
-         return AWS.Response.Build ("text/html", "<p>Hello world !");
+         return AWS.Response.Acknowledge(Status_Code => AWS.Messages.S404,
+                                     Message_Body => "404 Not found",
+                                     Content_Type => AWS.MIME.Text_Plain);
       end if;
 
    end CallBack;
